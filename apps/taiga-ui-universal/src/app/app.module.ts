c70de50a99976451e0929865d70cc9b6d4156f9b
dren0r123 <dren0risme@gmail.com>
2022-06-01 14:22:44 +0300

    init

--- apps/taiga-ui-universal/src/app/app.module.ts
+++ apps/taiga-ui-universal/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TuiRootModule } from '@taiga-ui/core';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    TuiRootModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ apps/taiga-ui-universal/src/app/app.server.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { ServerModule } from '@angular/platform-server';
+import { UniversalModule } from '@ng-web-apis/universal';
+
+import { AppModule } from './app.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [
+    AppModule,
+    ServerModule,
+    UniversalModule
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppServerModule {}
